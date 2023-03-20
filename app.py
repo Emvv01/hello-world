8a006c43f936281ba1e8988001dcc6638f18ee88
Emvv01 <54835287+Emvv01@users.noreply.github.com>
2023-03-20 15:12:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ import streamlit as st
 
 st.title("hi")
 
+option = st.selectbox(
+'How would you like to be contacted?',
+('Email', 'Home phone', 'Mobile phone'))
+
+st.write('You selected:', option)
+
 if st.button('Say hello'):
     st.write('Why hello there')
 else:
     st.write('Goodbye')
     
     
-option = st.selectbox(
-'How would you like to be contacted?',
-('Email', 'Home phone', 'Mobile phone'))
 
-st.write('You selected:', option)
