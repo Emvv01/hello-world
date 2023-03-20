af47e501945e9df2c998988bbebd86cb2ab729df
Emvv01 <54835287+Emvv01@users.noreply.github.com>
2023-03-20 10:59:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 import streamlit as st
+import streamlit as st
+import pandas as pd
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import re
+import string
+import nltk
+from nltk.corpus import stopwords
+lemmatizer = nltk.stem.WordNetLemmatizer()
+
+st.set_page_config(layout="wide")
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+df = pd.read_csv('listings.csv')
 
 st.title("hi")
