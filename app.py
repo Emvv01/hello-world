9c456ebdbbd3588ca12cbb32177bdf547b21ab84
Emvv01 <54835287+Emvv01@users.noreply.github.com>
2023-03-20 15:24:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,5 +13,15 @@ if st.button('Say hello'):
 else:
     st.write('Goodbye')
     
-    
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
 
